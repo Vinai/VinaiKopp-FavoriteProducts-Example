ed7702c9200f193f549f87b433113323b1485730
Vinai Kopp <vinai@netzarbeiter.com>
2018-05-21 14:12:10 +0200

    Refactor to get rid of obsolete refresh-on-delete call

--- view/frontend/web/js/favorite-products.js
+++ view/frontend/web/js/favorite-products.js
@@ -1,13 +1,13 @@
 define(['jquery', 'Magento_Customer/js/customer-data'], function ($, customer_data) {
-    "use strict";
+    'use strict';
     
     const section_name = 'vinaikopp_favoriteproducts';
-    var favoriteSkus = function () {
+    const favoriteSkus = function () {
         const favorites = customer_data.get(section_name);
         return favorites().skus || [];
     };
     
-    var isFavorite = function (sku) {
+    const isFavorite = function (sku) {
         return -1 !== favoriteSkus().indexOf(sku);
     };
     
@@ -17,8 +17,7 @@ define(['jquery', 'Magento_Customer/js/customer-data'], function ($, customer_da
             return function() {
                 const url = config.update_url + sku;
                 if (isFavorite(sku)) {
-                    customer_data.invalidate([section_name]);
-                    $.ajax(url, {method: 'DELETE'}).done(function() {customer_data.reload([section_name], true);});
+                    $.ajax(url, {method: 'DELETE'});
                 } else {
                     $.ajax(url, {method: 'POST'});
                 }
